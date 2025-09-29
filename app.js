ef5e21a463ef6d8dd70dc66fcbd78fc90d438aa1
Ket <lekiet2409@gmail.com>
2025-09-29 21:33:36 +0700

    Fix App.js
    Thêm việc file có thể đọc JSON

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/passportAuth");
 
 // Middleware
 app.set("view engine", "ejs");
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   session({
