52bb50647529b0d3ca85e6e4818c42cdb4185473
Ket <lekiet2409@gmail.com>
2025-09-29 21:35:31 +0700

    Fix App

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ mongoose.connect("mongodb://127.0.0.1:27017/passportAuth");
 
 // Middleware
 app.set("view engine", "ejs");
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   session({
